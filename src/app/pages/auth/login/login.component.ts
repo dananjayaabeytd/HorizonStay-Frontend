d3d6cfa402dc45571c36bacec05109f65a0d7f51
Tharindu Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2024-09-30 21:58:29 +0530

    feat: add user login logic

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -23,6 +23,37 @@ export class LoginComponent {
   errorMessage: string = '';
 
   async handleSubmit() {
+    if (!this.email || !this.password) {
+      this.showError('Email and Password is required');
+      return;
+    }
 
+    try {
+      const response = await this.usersService.login(this.email, this.password);
+      console.log(response);
+      if (response.statusCode == 200) {
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('role', response.role);
+        this.router.navigate(['/profile']);
+
+        this.usersService.setUserData({
+          userId: response.userId,
+          role: response.role,
+        })
+
+        console.log(this.usersService.getUserData());
+      } else {
+        this.showError(response.message);
+      }
+    } catch (error: any) {
+      this.showError(error.message);
+    }
+  }
+
+  showError(mess: string) {
+    this.errorMessage = mess;
+    setTimeout(() => {
+      this.errorMessage = '';
+    }, 3000);
   }
 }
