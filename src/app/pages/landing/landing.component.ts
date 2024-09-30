125400b70c42911cd33cdb40c94cac254b60e82b
Tharindu Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2024-09-30 21:44:25 +0530

    feat: complete landing page

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
+import { LocationsComponent } from "./locations/locations.component";
+import { WhyComponent } from "./why/why.component";
+import { FaqComponent } from "./faq/faq.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, LocationsComponent, WhyComponent, FaqComponent],
   templateUrl: './landing.component.html',
 })
 export class LandingComponent {
