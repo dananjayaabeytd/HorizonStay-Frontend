7bc849c234a620b680860dfce4d60368613a2561
Tharindu Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2024-09-30 22:05:21 +0530

    feat: add user profile logic

--- src/app/pages/user/profile/profile.component.ts
+++ src/app/pages/user/profile/profile.component.ts
@@ -14,8 +14,31 @@ export class ProfileComponent implements OnInit {
     private readonly userService: UsersService,
     private readonly router: Router
   ) {}
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+
+  profileInfo: any;
+  errorMessage: string = '';
+
+  async ngOnInit() {
+    try {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        throw new Error('No Token Found');
+      }
+
+      this.profileInfo = await this.userService.getYourProfile(token);
+    } catch (error: any) {
+      this.showError(error.message);
+    }
   }
 
+  updateProfile(id: string) {
+    this.router.navigate(['/update', id]);
+  }
+
+  showError(mess: string) {
+    this.errorMessage = mess;
+    setTimeout(() => {
+      this.errorMessage = '';
+    }, 3000);
+  }
 }
