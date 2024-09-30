bc1754f75b5989c9cc36314f18f04700ee134257
Tharindu Dananjaya Abenayaka <dananjayaabeytd@gmail.com>
2024-09-30 22:09:17 +0530

    feat: add user update logic

--- src/app/pages/user/updateuser/updateuser.component.ts
+++ src/app/pages/user/updateuser/updateuser.component.ts
@@ -9,7 +9,6 @@ import { Router, ActivatedRoute } from '@angular/router';
   standalone: true,
   imports: [CommonModule,FormsModule],
   templateUrl: './updateuser.component.html',
-
 })
 export class UpdateuserComponent {
  
@@ -18,9 +17,66 @@ export class UpdateuserComponent {
     private readonly router: Router,
     private readonly route:ActivatedRoute){}
 
+
     userId: any;
     userData: any = {}
     errorMessage:string = ''
 
+
+  ngOnInit(): void {
+    this.getUserById()
+  }
+
+  async getUserById(){
+      this.userId = this.route.snapshot.paramMap.get('id')
+      const token = localStorage.getItem('token')
+      if(!this.userId || !token){
+          this.showError("User ID or Token is Required")
+          return;
+      }
+
+      try {
+        let userDataResponse = await this.userService.getUsersById(this.userId, token)
+        const {name, email,role ,address,nic,password,image} = userDataResponse.systemUsers
+        this.userData = {name, email,role ,address,nic,password,image};
+        
+      } catch (error:any) {
+        this.showError(error.message);
+      }
+  }
+
+  async updateUser(){
+
+    const confirmm = confirm("Are you sure you wanna update this user")
+    if(!confirm) return
+
+    try{
+      const token = localStorage.getItem('token')
+      if(!token){
+        throw new Error("Token not found")
+      }
+      
+      const res = await this.userService.updateUser(this.userId, this.userData, token);
+      console.log(res)
+
+      if(res.statusCode === 200){
+        this.router.navigate(['/users'])
+      }else{
+        this.showError(res.message)
+      }
+
+    }catch(error:any){
+      this.showError(error.message)
+    }
+
+  }
+
+
+  showError(mess: string) {
+    this.errorMessage = mess;
+    setTimeout(() => {
+      this.errorMessage = ''
+    }, 3000)
+  }
 
 }
