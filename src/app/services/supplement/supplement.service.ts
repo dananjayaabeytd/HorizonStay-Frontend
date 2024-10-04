cf8cfd9a5869f348bd97d5288e6b170e092791ea
Tharindu Dananjaya Abenayaka <155418943+dananjayaabeytd@users.noreply.github.com>
2024-10-04 18:11:33 +0530

    feat(supplement): impl supplement service

--- src/app/services/supplement/supplement.service.ts
+++ src/app/services/supplement/supplement.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SupplementService {
+  private BASE_URL = 'http://localhost:5000';
+  
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  // Add a new supplement to a season
+  addSupplementToSeason(seasonID: number, supplementData: any, token: string): Observable<any> {
+    const url = `${this.BASE_URL}/supplement/${seasonID}`;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    });
+    return this.http.post(url, supplementData, { headers });
+  }
+
+  // Get supplement by ID
+  getSupplementById(supplementID: number, token: string): Observable<any> {
+    const url = `${this.BASE_URL}/supplement/${supplementID}`;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    });
+    return this.http.get(url, { headers });
+  }
+
+  // Update a supplement by ID
+  updateSupplement(supplementID: number, supplementData: any, token: string): Observable<any> {
+    const url = `${this.BASE_URL}/supplement/update/${supplementID}`;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    });
+    return this.http.put(url, supplementData, { headers });
+  }
+
+  // Delete a supplement by ID
+  deleteSupplement(supplementID: number, token: string): Observable<any> {
+    const url = `${this.BASE_URL}/supplement/delete/${supplementID}`;
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+    return this.http.delete(url, { headers });
+  }
+
+  // Get all supplements by season ID
+  getSupplementsBySeasonId(seasonID: number, token: string): Observable<any> {
+    const url = `${this.BASE_URL}/supplement/season/${seasonID}`;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    });
+    return this.http.get(url, { headers });
+  }
 }
