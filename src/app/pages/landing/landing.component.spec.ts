07b809a66c744ba389975b825c9f1719149ca78d
Tharindu Dananjaya Abenayaka <155418943+dananjayaabeytd@users.noreply.github.com>
2024-11-20 14:55:32 +0530

    test(landing): add test cases for landing component

--- src/app/pages/landing/landing.component.spec.ts
+++ src/app/pages/landing/landing.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LandingComponent } from './landing.component';
+import { HeroComponent } from './hero/hero.component';
+import { LocationsComponent } from './locations/locations.component';
+import { WhyComponent } from './why/why.component';
+import { FaqComponent } from './faq/faq.component';
+import { SearchComponent } from './search/search.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('LandingComponent', () => {
   let component: LandingComponent;
@@ -8,9 +13,16 @@ describe('LandingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LandingComponent]
-    })
-    .compileComponents();
+      imports: [
+        HttpClientTestingModule,
+        LandingComponent,
+        HeroComponent,
+        LocationsComponent,
+        WhyComponent,
+        FaqComponent,
+        SearchComponent,
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LandingComponent);
     component = fixture.componentInstance;
